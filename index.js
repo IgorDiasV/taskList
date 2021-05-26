41f923e983d362d0db46f95a60146234cf8ed873
Matheus Santos <matheusslr@hotmail.com>
2021-05-26 18:40:01 -0300

    'bancoFuncional'

--- index.js
+++ index.js
@@ -16,6 +16,28 @@ const task = require('./models/task')
     app.get('/', function(req,res){
         res.render('home')
     })
+    app.post('/addTarefa',function(req,res){
+        task.create({
+            nomes:req.body.nomes,
+            horas:req.body.horas,
+            intervalos:req.body.intervalos,
+            replays:req.body.replays=='on',
+            domingos:req.body.domingos=='on',
+            segundas:req.body.segundos=='on',
+            tercas:req.body.tercas=='on',
+            quartas:req.body.quartas=='on',
+            quintas:req.body.quintas=='on',
+            sextas:req.body.sextas=='on',
+            sabados:req.body.sabados=='on'
+        }).then(function()
+        {
+           res.redirect('/')
+        }).catch(function(erro)
+        {
+            if(req.body.domingos == 'on')
+                console.log("ocorreu o seguinte erro: "+ req.body.domingos)
+        })
+    })
 
 app.listen(12000, function(){
     console.log("Servidor rodando na url http://localhost:12000");
